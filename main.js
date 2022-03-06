36749f426537b69e2cc90b38598ce64057a9ac21
Vinicius Menezes <vini.ads13@outlook.com>
2022-03-06 19:47:53 -0300

    :pencil: modified the style and index with services and about files

--- main.js
+++ main.js
@@ -17,3 +17,18 @@ for (link of links) {
     nav.classList.remove('show')
   })
 }
+
+/* mudar o header da página quando der scroll */
+
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= navHeight) {
+    //scroll mais do que a altura do header
+    header.classList.add('scroll')
+  } else {
+    //menor que a altura do header
+    header.classList.remove('scroll')
+  }
+})
